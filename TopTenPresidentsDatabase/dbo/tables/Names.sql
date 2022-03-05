4556b3af2dcfb2dfbd1257e25e4a6e4973438858
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2022-03-05 17:42:38 -0500

    Remove database project.

--- TopTenPresidentsDatabase/dbo/tables/Names.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Names]
-(
-	[Id] SMALLINT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [MiddleName] NVARCHAR(50) NOT NULL,
-    [LastName] NVARCHAR(50) NOT NULL,
-    [DateOfBirth] DATE NOT NULL,
-    [DateOfDeath] DATE NULL,
-)
--- TopTenPresidentsDatabase/dbo/tables/Terms.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Terms]
-(
-	[Id] SMALLINT NOT NULL PRIMARY KEY,
-    [OfficeId] SMALLINT NOT NULL,
-    [ElectedDate] DATE NOT NULL, 
-    [InaugurationDate] DATE NOT NULL, 
-    [LastDate] DATE NULL, 
-    CONSTRAINT [FK_Terms_Offices] FOREIGN KEY ([OfficeId]) REFERENCES [Offices]([Id])
-)
